e238fd610946ca6a2b5efa65f5817beda302d13d
awais97 <awaishassan1997@gmail.com>
2024-02-19 21:45:15 +0500

    Created Page UI is completed

--- client/restaurant-dashboard/src/app/(routes)/create-food/page.tsx
+++ client/restaurant-dashboard/src/app/(routes)/create-food/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import CreateFood from "../../../shared/modules/dashboard/Create.food"
 
-const CreateFood = () => {
+const CreateFoodPage = () => {
   return (
-    <div>CreateFood</div>
+    <div className="w-full">
+      <h1 className="text-4xl text-center pt-5">Create Food</h1>
+      <CreateFood />
+    </div>
   )
 }
 
-export default CreateFood
+export default CreateFoodPage
