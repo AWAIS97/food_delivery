429078682238f4357ecdeb9fe2f9e0a15ac443d1
awais97 <awaishassan1997@gmail.com>
2024-02-19 21:44:44 +0500

    Invoice Page added in Restuarant Dashboard

--- client/restaurant-dashboard/src/shared/modules/dashboard/Dashboard.data.tsx
+++ client/restaurant-dashboard/src/shared/modules/dashboard/Dashboard.data.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import InvoiceCharts from "../../components/charts/Invoice.charts";
+import OrdersData from "../../components/data/Order.data"
 
 const DashboardData = () => {
   return (
-    <div>DashboardData</div>
-  )
-}
+    <div className="w-full flex p-8 items-center justify-between">
+      <div className="w-[56%]">
+        <h3 className="text-2xl pb-2">Recents Orders</h3>
+        <OrdersData isDashboard={true} />
+      </div>
+      <div className="w-[43%]">
+        <h3 className="text-2xl mb-[-2rem]">Orders Analytics</h3>
+        <InvoiceCharts />
+      </div>
+    </div>
+  );
+};
 
-export default DashboardData
+export default DashboardData;
