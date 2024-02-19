c3970bb4ab8f3b8d76758d145b8cc40a7d6a4439
awais97 <awaishassan1997@gmail.com>
2024-02-19 21:43:49 +0500

    Orders Page in Restuarant Dashboard

--- client/restaurant-dashboard/src/app/(routes)/orders/page.tsx
+++ client/restaurant-dashboard/src/app/(routes)/orders/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import OrdersData from "../../../shared/components/data/Order.data";
 
 const Orders = () => {
   return (
-    <div>Orders</div>
-  )
-}
+    <div className="md:w-[90%] xl:w-[75%] m-auto">
+      <h1 className="text-4xl text-center pt-3">All Orders</h1>
+      <OrdersData isDashboard={false} />
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
